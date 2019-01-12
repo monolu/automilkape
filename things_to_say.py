e878467182e1408f6e38af818d47d7049b5103ed
monolu <45496297+monolu@users.noreply.github.com>
2019-01-13 00:22:11 +0100

    retraining

--- things_to_say.py
+++ things_to_say.py
@@ -10,13 +10,28 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-while True:
-    from textgenrnn import textgenrnn
-    t = textgenrnn('textgenrnn_weights.hdf5')
+from textgenrnn import textgenrnn
+t = textgenrnn('textgenrnn_weights.hdf5')
 
-    to_tweet = ''.join(t.generate(n=1, return_as_list=True))
-    str1 = ''.join(to_tweet)
+to_tweet = ''.join(t.generate(n=1, temperature=1.0, return_as_list=True))
+str1 = ''.join(to_tweet)
 
-    api.update_status(status = str1)
-    time.sleep(21600)
+for tweet in tweepy.Cursor(api.search, q='@ApeAutomated', lang = 'en').items(1):
+    try:
+        print ("Found tweet by:@" + tweet.user.screen_name)
+        api.update_status(status = '@' + tweet.user.screen_name + ' ' + str1, in_reply_to_status_id = tweet.id_str)
+        print ('responded to @' + tweet.user.screen_name)
+        
+    except tweepy.TweepError as e:
+        print(e.reason)
+        time.sleep(3)
+        continue
+    except tweepy.RateLimitError:
+        time.sleep(15*60)
+    except StopIteration:
+        break
+
+# while True:
+#     api.update_status(status = str1)
+#     time.sleep(21600)
 
--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 from textgenrnn import textgenrnn
-t = textgenrnn('textgenrnn_weights.hdf5')
-t.train_from_file('ApeMiIk_tweets.txt', num_epochs=20)
+t = textgenrnn()
+t.train_from_file('ApeMiIk_tweets.txt', num_epochs=150)
